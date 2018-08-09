6154ea3a070f9bf66e0ddd50730b82b205c451f6
Pomian <kontakt@rapiddev.pl>
2018-08-10 01:00:07 +0200

    Update

--- polishpost_api/polishpost_api.php
+++ polishpost_api/polishpost_api.php
@@ -11,10 +11,6 @@
  *
  * This file is part of the Polish Post tracking API.
  *
- * (c) RapidDev Leszek Pomianowski <https://rapiddev.pl>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
  * More info about API and documentation can be found on:
  * http://www.poczta-polska.pl/webservices/
  *
@@ -22,14 +18,61 @@
 	if (!class_exists('PolishPostAPI')) {
 		class PolishPostAPI
 		{
-			private $username, $password, $request_url, $request_body, $request_urn, $curl;
 
+			/**
+			* Data for the request, username
+			* @var string
+			* @access private
+			*/
+			private $username;
+
+			/**
+			* Data for the request, password
+			* @var string
+			* @access private
+			*/
+			private $password;
+
+			/**
+			* Data for the request, URL
+			* @var string
+			* @access private
+			*/
+			private $request_url;
+
+			/**
+			* The values passed to the request
+			* @var string
+			* @access private
+			*/
+			private $request_body;
+
+			/**
+			* ID of the WSDL request function
+			* @var string
+			* @access private
+			*/
+			private $request_urn;
+
+			/**
+			* constructor
+			*
+			* @param string $username in most cases the default should work
+			* @param string $password in most cases the default should work
+			*/
 			public function __construct($username = 'sledzeniepp', $password = 'PPSA')
 			{
 				$this->username = $username;
 				$this->password = $password;
 			}
 
+			/**
+			* get_package
+			*
+			* @return	array	Correct data table
+			* @param    string	$package package number, no spaces or brackets, the default (test) should work
+			* @access   public
+			*/
 			public function get_package($package = 'testp0')
 			{
 				$this->request_url = 'http://tt.poczta-polska.pl/Sledzenie/services/Sledzenie.SledzenieHttpSoap11Endpoint/';
@@ -39,6 +82,13 @@
 				return $this->parse_xml($this->curl());
 			}
 
+			/**
+			* parse_xml
+			*
+			* @return	array	Correct data table
+			* @param    string	$xml_data xml data is prepared for processing and then changed from object to table
+			* @access   private
+			*/
 			private function parse_xml($xml_data)
 			{
 				if (strpos($xml_data, 'soapenv:') !== false && function_exists('simplexml_load_string'))
@@ -52,21 +102,27 @@
 				
 			}
 
+			/**
+			* curl
+			*
+			* @return	array/null	Gets data from Poczta Polska, and then returns XML as a STRING. Only verifies CURL errors
+			* @access   private
+			*/
 			private function curl()
 			{
 				$XML_REQUEST = '<?xml version="1.0" encoding="UTF-8"?><SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="http://sledzenie.pocztapolska.pl" xmlns:ns2="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"><SOAP-ENV:Header><ns2:Security SOAP-ENV:mustUnderstand="1"><ns2:UsernameToken><ns2:Username>'.$this->username.'</ns2:Username><ns2:Password>'.$this->password.'</ns2:Password></ns2:UsernameToken></ns2:Security></SOAP-ENV:Header><SOAP-ENV:Body><ns1:'.$this->request_urn.'>'.$this->request_body.'</ns1:'.$this->request_urn.'></SOAP-ENV:Body></SOAP-ENV:Envelope>';
-				$this->curl = curl_init();
-				curl_setopt($this->curl, CURLOPT_URL, $this->request_url);
-				curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
-				curl_setopt($this->curl, CURLOPT_POST, true);
-				curl_setopt($this->curl, CURLOPT_POSTFIELDS, $XML_REQUEST);
-				curl_setopt($this->curl, CURLOPT_HTTPHEADER,array('Connection: Keep-Alive','User-Agent: PHP-SOAP/7.1.1','Content-Type: text/xml; charset=utf-8','SOAPAction: "urn:'.$this->request_urn.'"','Content-length: '.strlen($XML_REQUEST)));
-				$result = curl_exec($this->curl);
-				if (curl_error($this->curl))
+				$SOAP_CURL = curl_init();
+				curl_setopt($SOAP_CURL, CURLOPT_URL, $this->request_url);
+				curl_setopt($SOAP_CURL, CURLOPT_RETURNTRANSFER, true);
+				curl_setopt($SOAP_CURL, CURLOPT_POST, true);
+				curl_setopt($SOAP_CURL, CURLOPT_POSTFIELDS, $XML_REQUEST);
+				curl_setopt($SOAP_CURL, CURLOPT_HTTPHEADER,array('Connection: Keep-Alive','User-Agent: PHP-SOAP/7.1.1','Content-Type: text/xml; charset=utf-8','SOAPAction: "urn:'.$this->request_urn.'"','Content-length: '.strlen($XML_REQUEST)));
+				$result = curl_exec($SOAP_CURL);
+				if (curl_error($SOAP_CURL))
 				{
-					$error_msg = curl_error($this->curl);
+					$error_msg = curl_error($SOAP_CURL);
 				}
-				curl_close($this->curl);
+				curl_close($SOAP_CURL);
 				if (isset($error_msg))
 				{
 					return $error_msg;
@@ -78,4 +134,5 @@
 			}
 		}
 	}
+
 ?>
