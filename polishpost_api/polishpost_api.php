7324f39cc99c916701ad81c9ea2b3d239ce27a6f
Pomian <kontakt@rapiddev.pl>
2018-08-10 00:22:10 +0200

    Add files via upload

---
+++ polishpost_api/polishpost_api.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Polish post tracking API.
+ *
+ * More info about API and documentation can be found on: http://www.poczta-polska.pl/webservices/
+ */
+
+	if (!class_exists('polishPostApi')) {
+		class polishPostApi
+		{
+			
+		}
+	}
+
+?>
---
+++ polishpost_api/readme.txt
@@ -0,0 +1,3 @@
+Class to tracking package sent by Polish Post Office.
+=====================================================
+Klasa do śledzenia przesyłki wysłanej poprzez Poczte Polską.
